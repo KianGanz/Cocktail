20a1890d4538fc5d79a4eb1adc3352175d6cbc21
Leon Heid <88842858+LeonHeid@users.noreply.github.com>
2022-02-07 14:30:15 +0000

    API inbound and working

--- app.js
+++ app.js
@@ -1,19 +1,12 @@
-// Create a request variable and assign a new XMLHttpRequest object to it.
-var request = new XMLHttpRequest()
+const userAction = async () => {
 
-// Open a new connection, using the GET request on the URL endpoint
-request.open('GET', 'https://thecocktaildb.com/api/json/v1/1/search.php?s=margarita', true)
+    const response = await fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita');
 
-request.onload = function () {
-  // Begin accessing JSON data here
+    const myJson = await response.json();
+
+    console.log(myJson.drinks)
+
 }
 
-// Send request
-request.send()
-// Begin accessing JSON data here
-var data = JSON.parse(this.response)
 
-data.forEach(drinks => {
-  // Log each movie's title
-  console.log(strIngredient)
-})
+userAction()
