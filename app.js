00f466be2f170d77546b80e6431f5ef182c7da52
Luke Meier Meier <73538499+LukeMeier@users.noreply.github.com>
2022-02-10 07:31:09 +0000

    Update about us and Picture

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
-function pic 
+function pic () {
 let zahl = Math.ceil(Math.random() * 8);
+let zahlp = Math.ceil(Math.random() * 8);
 let imgf = "<img id=imgleft src=./picture/"+ zahl + ".png>";
-let imgl = "<img id=imgright src=./picture/"+ zahl + ".png>";
+let imgl = "<img id=imgright src=./picture/"+ zahlp + ".png>";
 let giv = document.getElementById('bild');
-giv.insertAdjacentHTML("beforeend", "imgf");
-giv.insertAdjacentHTML("beforeend", "imgl");
+giv.insertAdjacentHTML("beforeend", imgf);
+giv.insertAdjacentHTML("beforeend", imgl);
+}
+pic();
--- search/search.js
+++ search/search.js
@@ -3,15 +3,18 @@ async function getInfo (inputText) {
   const response = await fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=' + inputText);
 
     var myJson = await response.json();
+    for (var key in myJson['drinks']) {
     const h2 = document.createElement('p');
-    console.log(myJson['drinks'][0]['strDrink'])
-    const text = document.createTextNode(myJson['drinks'][0]['strDrink']);
+    const text = document.createTextNode(myJson['drinks'][key]['strDrink']);
     console.log(text)
     const print = document.getElementById('ausgabe');
+    print.setAttribute("id", "ausgabe");
     h2.appendChild(text);
     print.appendChild(h2);
+    }
   }
 function knopf() {
+
   bila = document.getElementById("search").value;
   getInfo(bila);
 }
