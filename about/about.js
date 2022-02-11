2cfedf025dbe3343f2e40bcecbf6f98886d6d22d
Kank <89902597+KianGanz@users.noreply.github.com>
2022-02-11 09:07:48 +0000

    Images content. CSS.

--- about/about.js
+++ about/about.js
@@ -1,10 +1,16 @@
 var i = 0;
 var b = 0;
 var c = 0;
-var txt = 'We made this project for our Company the ZLI. The project idea started when Leon found the *thecocktailDB* and he then came up with the idea to turn it into a website where you can search for drinks or get recommended a drink. After the idea was presented Luke and Kian joined the project and wanted to help make it happen. The group then started to plan how they are going to turn the idea into a real thing. After a short planing phase Kian quickly took the lead of the group and started to distribute the many tasks that were needed to do. After the group has done the planing they started by creating the design on how the website should look at the end. After they decided on how it should look like they started on creating the website. But just after a short time there was problem. The problem was the API since they were strugling to include it into the project while displaying the right data they wanted. But after some help they had the problem solved and they started to do the *search a drink* function. After creating the search function they had the idea to create a function which would recommend you a drink to try. They quickly started on creating the *Recommend me a drink* function. While Kian and Leon were working on the recommend function Luke started to improve the design of the *Homepage* by including pictures of drinks. While including the pictures the was still the problem of them being formatted wrong. But soon after they found the problem and fixed the formating of the images. Luke continued on finishing the search function while Leon made the recommend function and Kian was doing some small changes of the standard design of the Homepage. Leon then came up with the idea of creating this Page where he wrote about this Project. We hope that you like our little Project "The Web-Bar". Written by: Leon. ';
-var txt2 = 'The project idea started when Leon found the thecocktailDB';
-var txt3 = "";
-var speed = 75;
+var d = 0;
+var e = 0;
+var f = 0;
+var txt = 'We made this project for our Company the ZLI. ';
+var txt2 = 'The project idea started when Leon found the thecocktailDB ';
+var txt3 = "and he then came up with the idea to turn it into a website where you can search for drinks or get recommended a drink. After the idea was presented Luke and Kian joined the project and wanted to help make it happen. The group then started to plan how they are going to turn the idea into a real thing. After a short planing phase Kian quickly took the lead of the group and started to distribute the many tasks that were needed to do. After the group has done the planing they started by creating the design on how the website should look at the end. After they decided on how it should look like they started on creating the website. But just after a short time there was problem. The problem was the API since they were strugling to include it into the project while displaying the right data they wanted. But after some help they had the problem solved and they started to do the search a drink ";
+var txt4 = "function. After creating the search function they had the idea to create a function which would recommend you a drink to try. They quickly started on creating the Recommend me a drink ";
+var txt5 = "function. While Kian and Leon were working on the recommend function Luke started to improve the design of the Homepage  ";
+var txt6 = "by including pictures of drinks. While including the pictures the was still the problem of them being formatted wrong. But soon after they found the problem and fixed the formating of the images. Luke continued on finishing the search function while Leon made the recommend function and Kian was doing some small changes of the standard design of the Homepage. Leon then came up with the idea of creating this Page where he wrote about this Project. We hope that you like our little Project \"The Web-Bar\". Written by: Leon.";
+var speed = 50;
 
 function typeWriter() {
   if (i < txt.length) {
@@ -12,18 +18,17 @@ function typeWriter() {
     i++;
     setTimeout(typeWriter, speed);
   } else {
-    setTimeout(500);
   document.getElementById("demo").innerHTML = "We made this project for our Company the <a href='https://www.zli.ch'>ZLI</a>. ";
   setTimeout(typeWriter2, 200)
   }
 } 
 function typeWriter2() {
-  if (b < txt2.length) {
+ if (b < txt2.length) {
     document.getElementById("demo").innerHTML += txt2.charAt(b);
     b++;
     setTimeout(typeWriter2, speed);
   } else {
-  document.getElementById("demo").innerHTML = "We made this project for our Company the <a href='https://www.zli.ch'>ZLI</a>. The project idea started when Leon found the <a href='https://www.thecocktaildb.com/'>thecocktailDB</a>";
+  document.getElementById("demo").innerHTML = "We made this project for our Company the <a href='https://www.zli.ch'>ZLI</a>. The project idea started when Leon found the <a href='https://www.thecocktaildb.com/'>thecocktailDB</a> ";
   setTimeout(typeWriter3, 200);
   }
 }
@@ -32,6 +37,36 @@ function typeWriter3() {
     document.getElementById("demo").innerHTML += txt3.charAt(c);
     c++;
     setTimeout(typeWriter3, speed);
+  } else {
+    document.getElementById("demo").innerHTML = "We made this project for our Company the <a href='https://www.zli.ch'>ZLI</a>. The project idea started when Leon found the <a href='https://www.thecocktaildb.com/'>thecocktailDB</a> and he then came up with the idea to turn it into a website where you can search for drinks or get recommended a drink. After the idea was presented Luke and Kian joined the project and wanted to help make it happen. The group then started to plan how they are going to turn the idea into a real thing. After a short planing phase Kian quickly took the lead of the group and started to distribute the many tasks that were needed to do. After the group has done the planing they started by creating the design on how the website should look at the end. After they decided on how it should look like they started on creating the website. But just after a short time there was problem. The problem was the API since they were strugling to include it into the project while displaying the right data they wanted. But after some help they had the problem solved and they started to do the <a href='./../search/Search.html'>search a drink</a> ";
+  setTimeout(typeWriter4, 200);
+  }
+}
+function typeWriter4() {
+  if (d < txt4.length) {
+    document.getElementById("demo").innerHTML += txt4.charAt(d);
+    d++;
+    setTimeout(typeWriter4, speed);
+  } else {
+    document.getElementById("demo").innerHTML = "We made this project for our Company the <a href='https://www.zli.ch'>ZLI</a>. The project idea started when Leon found the <a href='https://www.thecocktaildb.com/'>thecocktailDB</a> and he then came up with the idea to turn it into a website where you can search for drinks or get recommended a drink. After the idea was presented Luke and Kian joined the project and wanted to help make it happen. The group then started to plan how they are going to turn the idea into a real thing. After a short planing phase Kian quickly took the lead of the group and started to distribute the many tasks that were needed to do. After the group has done the planing they started by creating the design on how the website should look at the end. After they decided on how it should look like they started on creating the website. But just after a short time there was problem. The problem was the API since they were strugling to include it into the project while displaying the right data they wanted. But after some help they had the problem solved and they started to do the <a href='./../search/Search.html'>search a drink</a> function. After creating the search function they had the idea to create a function which would recommend you a drink to try. They quickly started on creating the <a href='./../recommended-drink/recommended-drink.html'>Recommend me a drink</a> ";
+  setTimeout(typeWriter5, 200);
+  }
+}
+function typeWriter5() {
+  if (e < txt5.length) {
+    document.getElementById("demo").innerHTML += txt5.charAt(e);
+    e++;
+    setTimeout(typeWriter5, speed);
+  } else {
+    document.getElementById("demo").innerHTML = "We made this project for our Company the <a href='https://www.zli.ch'>ZLI</a>. The project idea started when Leon found the <a href='https://www.thecocktaildb.com/'>thecocktailDB</a> and he then came up with the idea to turn it into a website where you can search for drinks or get recommended a drink. After the idea was presented Luke and Kian joined the project and wanted to help make it happen. The group then started to plan how they are going to turn the idea into a real thing. After a short planing phase Kian quickly took the lead of the group and started to distribute the many tasks that were needed to do. After the group has done the planing they started by creating the design on how the website should look at the end. After they decided on how it should look like they started on creating the website. But just after a short time there was problem. The problem was the API since they were strugling to include it into the project while displaying the right data they wanted. But after some help they had the problem solved and they started to do the <a href='./../search/Search.html'>search a drink</a> function. After creating the search function they had the idea to create a function which would recommend you a drink to try. They quickly started on creating the <a href='./../recommended-drink/recommended-drink.html'>Recommend me a drink</a> function. While Kian and Leon were working on the recommend function Luke started to improve the design of the <a href='./../index.html'>Homepage</a> ";
+  setTimeout(typeWriter6, 200);
+  }
+}
+function typeWriter6() {
+  if (f < txt6.length) {
+    document.getElementById("demo").innerHTML += txt6.charAt(f);
+    f++;
+    setTimeout(typeWriter6, speed);
   } else {
   }
 }
---
+++ sides/4.js
@@ -0,0 +1,21 @@
+  async function Drink() {
+
+  const response = await fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=Aperol Spritz');
+
+    var myJson = await response.json();
+    let print = document.getElementById('outprint');
+  print.setAttribute("id", "outprint")
+  for (var key in myJson['drinks'][0]) {
+    if (myJson['drinks'][0][key] !== null) {
+    let h2 = document.createElement('p');
+    h2.setAttribute("class", "drink")
+    let text = document.createTextNode(key +": " +  myJson['drinks'][0][key]);
+    h2.appendChild(text);
+    print.appendChild(h2)
+    }
+    else {
+      console.log("Leon der Hurensohn")
+    }
+  }
+}
+Drink();
